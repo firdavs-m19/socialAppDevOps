343df895836a0ccf04baecf2ff522d1cfdbf36aa
firdavs-m19 <firdavsm19012005@gmail.com>
2025-05-02 15:06:03 +0500

    Social App

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ import storyRoute from "./routes/story.routes.js";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 const MONGO_URL = process.env.MONGO_URL;
 
